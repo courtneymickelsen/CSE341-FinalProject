925d022a97f3edebcd3e4928a62cfc49531815a7
Kofi Junior Eshun <eshunkofi23@gmail.com>
2022-11-10 19:57:32 -0700

    STarter Project created by Kofi

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.all("/*", (req,res) => {
+    res.send("404 Page Not found.\n It could be possible that this resource does not exist or was never created.");
+});
+
+app.listen((PORT), ()=>{
+    console.log("This server is listening on http://localhost:" + PORT);
+});
+
